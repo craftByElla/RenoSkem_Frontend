b22b685eff4387968c0f0e9a622496a6997b4e94
craftByElla <ella.thomas.mail@gmail.com>
2024-05-20 14:19:17 +0200

    maj iconbutton

--- components/buttons/IconButton.jsx
+++ components/buttons/IconButton.jsx
@@ -3,10 +3,10 @@ import { TouchableOpacity, StyleSheet } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 
-const IconButton = ({ onPress, style, iconName }) => {
+const IconButton = ({ onPress, style, iconName, color }) => {
   return (
     <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
-      <FontAwesome name={iconName} style={styles.icon} />
+      <FontAwesome name={iconName} style={[styles.icon, { color }]} />
     </TouchableOpacity>
   );
 };
@@ -15,6 +15,11 @@ IconButton.propTypes = {
   onPress: PropTypes.func.isRequired,
   style: PropTypes.object,
   iconName: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+IconButton.defaultProps = {
+  color: '#194852', // couleur par défaut
 };
 
 const styles = StyleSheet.create({
@@ -26,7 +31,6 @@ const styles = StyleSheet.create({
   },
   icon: {
     fontSize: 15,
-    color: 'black',
   },
 });
 
