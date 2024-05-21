bdf84e5406a7ed112be26d78b27a37ecf152a281
Your name <Your email>
2024-05-21 16:15:40 +0200

    teammateSkillsScreen

--- screens/team/TeammateSkillsScreen.jsx
+++ screens/team/TeammateSkillsScreen.jsx
@@ -1,143 +1,130 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity,ScrollView } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import IconButton from "../../components/buttons/IconButton";
 const picture = require("../../assets/Gabin.jpg");
+import PageTitle from "../../components/text/ScreenTitle";
+import FilledButton from "../../components/buttons/FilledButton";
+import { useTheme } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Stars from "../../components/buttons/Stars";
+import TextWithRadioButton from "../../components/buttons/TextWithRadioButtons"
+
+
+const postesTravaux = [
+    "Chauffage",
+    "Cloisonnement/Plâtrage",
+    "Démolition",
+    "Électricité",
+    "Étanchéité",
+    "Façade",
+    "Fondations",
+    "Installation cuisine/SDB",
+    "Isolation",
+    "Maçonnerie",
+    "Menuiserie",
+    "Montage de meuble",
+    "Peinture",
+    "Plomberie",
+    "Revêtements muraux",
+    "Revêtements sol",
+    "Revêtements extérieurs",
+    "Toiture",
+    "Ventilation"
+];
+
 
 export default function TeammateSkillsScreen({ navigation }) {
-    return (
-        <View style={styles.TeammateSkills}>
-            <TouchableOpacity onPress={() => navigation.goBack()}>
-                <FontAwesome name="arrow-left" style={styles.arrowLeft} />
-            </TouchableOpacity>
-            <View>
-                <Image source={picture} style={styles.picture} />
-            </View>
-            <TouchableOpacity onPress={() => console.log('Close')}>
-                <FontAwesome name="closecircle" style={styles.closecircle} />
-            </TouchableOpacity>
-            <View>
-                <Text style={styles.title}>Paramétrer le niveau d'expertise de Martin</Text>
-            </View>
-            <View style={styles.stars}>
-                <FontAwesome name="star" style={styles.starWhite} />
-                <FontAwesome name="star" style={styles.starOrange} />
-                <FontAwesome name="star" style={styles.starRed} />
-            </View>
-            <View>
-                <Text>Plomberie</Text>
-            </View>
-            <View>
-                <Text>Electricité</Text>
-            </View>
-            <View>
-                <Text>Menuiserie</Text>
-            </View>
-            <View>
-                <Text>Peinture</Text>
-            </View>
-            <View>
-                <Text>Revêtement de sol</Text>
-            </View>
-            <View>
-                <Text>Maçonnerie</Text>
-            </View>
-            <View>
-                <Text>Installation cuisine / SdB</Text>
-            </View>
-            <View>
-                <Text>Montage de meubles</Text>
-            </View>
-            <View>
-                <Text>Revêtement muraux</Text>
-            </View>
-            <View>
-                <Text>Cloisonnement/Plâtrage</Text>
-            </View>
-            <View>
-                <TouchableOpacity style={styles.enregistrer}>
-                    <Text>Enregistrer</Text>
-                </TouchableOpacity>
-            </View>
+  const { colors } = useTheme();
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.main}>
+      <View style={styles.header}>
+        <IconButton
+          style={styles.iconArrow}
+          onPress={() => navigation.navigate("HomeScreen")}
+          iconName="arrow-left"
+        />
+        
+          <Image source={picture} style={styles.picture} />
+        
+        <IconButton
+          style={styles.iconTimecircle}
+          onPress={() => navigation.navigate("HomeScreen")}
+          iconName="times-circle"
+        />
+      </View>
+      <View style={styles.h1}>
+        <PageTitle text="Paramétrer le niveau d'expertise" />
+      </View>
+      <Stars style={styles.stars}/>
+        <ScrollView style={styles.scrollableSection} contentContainerStyle={styles.scrollableContent}>
+        {postesTravaux.map((poste, index) => (
+        <TextWithRadioButton key={index} text={poste} />
+        ))}
+     </ScrollView>
+        <TouchableOpacity style={styles.enregistrer}>
+          <FilledButton
+            text="Enregistrer"
+            background={colors.deepGreen}
+            full={true}
+            onPress={() => navigation.navigate("TeamScreen")}
+          />
+        </TouchableOpacity>
         </View>
-    );
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    TeammateSkills: {
-        position: "relative",
-        width: 375,
-        height: 812,
-        backgroundColor: "#EFECEA",
-        borderRadius: 50,
+
+   main : {
+        height:'100%',
+        width:'100%',
+        justifyContent:'flex-start',
+        alignItems:'center',
     },
-    arrowLeft: {
-        position: "absolute",
-        width: 186.34,
-        height: 40.77,
-        left: 20,
-        top: 54,
-    },
-    picture: {
-        position: "absolute",
-        width: 68,
-        height: 64,
-        left: 149,
-        top: 42,
-        borderRadius: 50,
-    },
-    closecircle: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 12,
-        gap: 10,
-        position: "absolute",
-        width: 44,
-        height: 44,
-        left: 323,
-        top: 50,
-    },
-    title: {
-        position: "absolute",
-        width: 321,
-        height: 82,
-        left: 34,
-        top: 135,
-        fontFamily: 'Inter',
-        fontStyle: 'normal',
-        fontWeight: "800",
-        fontSize: 24,
-        lineHeight: 36,
-        letterSpacing: 0.15,
-        color: "#194852",
-    },
-    stars: {
-        position: "absolute",
-        top: 260,
-        left: 30,
-        right: 30,
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    starWhite: {
-        color: '#E76F51',
-    },
-    starOrange: {
-        color: 'rgba(231, 111, 81, 0.5)',
-    },
-    starRed: {
-        color: '#E76F51',
-    },
-    enregistrer: {
-        position: "absolute",
-        width: 314,
-        height: 49,
-        left: "calc(50% - 314/2 + 0.5)",
-        top: 735,
-        backgroundColor: '#194852',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 25,
-    },
+  picture: {
+    width: 68,
+    height: 64,
+    borderRadius: 50,
+    marginLeft:10,
+  },
+
+  h1: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginLeft: 30,
+    marginTop:50,
+  },
+
+  iconArrow:{
+    position: 'absolute', 
+        left: 20, 
+        top: '50%', 
+        marginTop: -25,
+        
+  },
+iconTimecircle: {
+    position: 'absolute', 
+        right: 20, 
+        top: '50%', 
+        marginTop: -25,
+},
+
+  header:{
+    flexDirection:"row",
+    justifyContent:"center",
+    width:'100%',
+    height:50,
+    position:'relative',
+
+
+  },
+  enregistrer: {
+    width: '100%',
+    alignItems: 'center'
+  }
+
 });
