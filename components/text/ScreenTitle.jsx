761b12bdcc48bc3e146abcc5a8ee649386159927
craftByElla <ella.thomas.mail@gmail.com>
2024-05-21 10:12:36 +0200

    ajout ScreenTitle avec majuscule

---
+++ components/text/ScreenTitle.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+const ScreenTitle = ({ text }) => {
+  return (
+    <Text style={styles.title}>
+      {text}
+    </Text>
+  );
+};
+
+ScreenTitle.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  title: {
+    width: '80%', // Occupe toute la largeur disponible
+    color: '#194852',
+    fontFamily: 'Inter',
+    fontSize: 24,
+    fontStyle: 'normal',
+    fontWeight: '800',
+    lineHeight: 36, 
+    letterSpacing: 0.15,
+    flexWrap: 'wrap', // Permet de faire passer le texte à la ligne
+    justifyContent: 'center',
+  },
+});
+
+export default ScreenTitle;
